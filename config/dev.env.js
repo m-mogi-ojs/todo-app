52e7fa6bd564c4cec71070e477b1cea5a98f170e
m-mogi-ojs <m.mogi.ojs@gmail.com>
2019-01-04 20:25:30 +0900

    update default environment variable

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,11 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  VUE_APP_FIREBASE_API_KEY: '',
+  VUE_APP_FIREBASE_AUTH_DOMAIN: '',
+  VUE_APP_FIREBASE_DATABASE_URL: '',
+  VUE_APP_FIREBASE_PROJECT_ID: '',
+  VUE_APP_FIREBASE_STORAGE_BUCKET: '',
+  VUE_APP_FIREBASE_MESSAGING_SENDER_ID: ''
 })
